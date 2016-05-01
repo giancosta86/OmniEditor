3783b28e65b3a8d8fa0ababa6b837e06a6c2ad82
Gianluca Costa <gianluca@gianlucacosta.info>
2016-05-01 22:56:48 +0200

    Update copyright notices

--- src/main/java/info/gianlucacosta/omnieditor/AtomicStringBuffer.java
+++ src/main/java/info/gianlucacosta/omnieditor/AtomicStringBuffer.java
@@ -2,7 +2,7 @@
   ===========================================================================
   OmniEditor
   ===========================================================================
-  Copyright (C) 2015 Gianluca Costa
+  Copyright (C) 2015-2016 Gianluca Costa
   ===========================================================================
   Licensed under the Apache License, Version 2.0 (the "License");
   you may not use this file except in compliance with the License.
